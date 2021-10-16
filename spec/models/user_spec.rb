95aee924793f77e3a30083102c4f2957b6fd17f5
takakuga <sazanami398@gmail.com>
2021-10-16 23:22:54 +0900

    contextを使用して特定の条件を指定してグループを分ける

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,9 +5,12 @@ RSpec.describe User, type: :model do
     @user = FactoryBot.build(:user)
   end
   describe 'ユーザー新規登録' do
+    context '新規登録できるとき' do
     it 'nicknameとemail、passwordとpassword_confirmationが存在すれば登録できる' do
       expect(@user).to be_valid
     end
+  end
+    context '新規登録できないとき' do
     it 'nicknameが空では登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -46,6 +49,7 @@ RSpec.describe User, type: :model do
       @user.valid?
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
+  end
   end
 end
 
