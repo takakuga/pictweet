6acc58ee139a0a0b6970acfbbee38e368d9c81a7
takakuga <sazanami398@gmail.com>
2021-10-18 19:14:44 +0900

    response.statusと実行し、正常にレスポンスを得るか確認

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -6,6 +6,8 @@ describe TweetsController, type: :request do
 
   describe 'GET #index' do
     it 'indexアクションにリクエストすると正常にレスポンスが返ってくる' do
+      get root_path
+      expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do
     end
