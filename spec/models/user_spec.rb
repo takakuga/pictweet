21e52578b9da45af5da6d2e45f4a0c149a198a52
takakuga <sazanami398@gmail.com>
2021-10-19 06:59:44 +0900

    ログインができるときのテストコードを記述

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -60,13 +60,25 @@ RSpec.describe 'ログイン', type: :system do
   context 'ログインができるとき' do
     it '保存されているユーザーの情報と合致すればログインできる' do
       # トップページに移動する
+      visit root_path
       # トップページにログインページへ遷移するボタンがあることを確認する
+      expect(page).to have_content('ログイン')
       # ログインページへ遷移する
+      visit new_user_session_path
       # 正しいユーザー情報を入力する
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: @user.password
       # ログインボタンを押す
+      find('input[name="commit"]').click
       # トップページへ遷移することを確認する
+      expect(current_path).to eq(root_path)
       # カーソルを合わせるとログアウトボタンが表示されることを確認する
+      expect(
+        find('.user_nav').find('span').hover
+      ).to have_content('ログアウト')
       # サインアップページへ遷移するボタンやログインページへ遷移するボタンが表示されていないことを確認する
+      expect(page).to have_no_content('新規登録')
+      expect(page).to have_no_content('ログイン')
     end
   end
   context 'ログインができないとき' do
