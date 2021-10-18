811b57c05911ba182f8b7c228faaace7a0cb4a92
takakuga <sazanami398@gmail.com>
2021-10-18 19:19:09 +0900

    indexアクションのテストコードを記述

--- spec/requests/tweets_spec.rb
+++ spec/requests/tweets_spec.rb
@@ -10,10 +10,16 @@ describe TweetsController, type: :request do
       expect(response.status).to eq 200
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートのテキストが存在する' do
+      get root_path
+      expect(response.body).to include(@tweet.text)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿済みのツイートの画像URLが存在する' do
+      get root_path
+      expect(response.body).to include(@tweet.image)
     end
     it 'indexアクションにリクエストするとレスポンスに投稿検索フォームが存在する' do
+      get root_path
+      expect(response.body).to include('投稿を検索する')
     end
   end
 end
