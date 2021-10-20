848c6f223b81a7fb6f4e8d801d145291a9436f77
takakuga <sazanami398@gmail.com>
2021-10-21 07:45:38 +0900

    ツイート詳細ページに遷移してコメント投稿欄が表示される場合を記述

--- spec/system/tweets_spec.rb
+++ spec/system/tweets_spec.rb
@@ -126,10 +126,22 @@ RSpec.describe 'ツイート詳細', type: :system do
   end
   it 'ログインしたユーザーはツイート詳細ページに遷移してコメント投稿欄が表示される' do
     # ログインする
+    visit new_user_session_path
+    fill_in 'Email', with: @tweet.user.email
+    fill_in 'Password', with: @tweet.user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq(root_path)
     # ツイートに「詳細」へのリンクがあることを確認する
+    expect(
+      all('.more')[0].hover
+    ).to have_link '詳細', href: tweet_path(@tweet)
     # 詳細ページに遷移する
+    visit tweet_path(@tweet)
     # 詳細ページにツイートの内容が含まれている
+    expect(page).to have_selector ".content_post[style='background-image: url(#{@tweet.image});']"
+    expect(page).to have_content("#{@tweet.text}")
     # コメント用のフォームが存在する
+    expect(page).to have_selector 'form'
   end
   it 'ログインしていない状態でツイート詳細ページに遷移できるもののコメント投稿欄が表示されない' do
     # トップページに移動する
